9d03bec3b3eae76f6d5fe0f230d761156c0069d8
Gustavo Silva <gustavofsilva@email.com>
2020-03-19 20:33:31 -0300

    04-ex04- done

--- 04-ex04/src/com/gustavo/Main.java
+++ 04-ex04/src/com/gustavo/Main.java
@@ -1,8 +1,19 @@
 package com.gustavo;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite uma frase:");
+        String frase = entrada.nextLine();
+        int quantidadeDePalavras;
+
+        System.out.println("Numero de caracteres: " + frase.length());
+
+        quantidadeDePalavras = frase.split(" ").length;
+        System.out.println("Quantidade de palavras: " + quantidadeDePalavras);
+
     }
 }
