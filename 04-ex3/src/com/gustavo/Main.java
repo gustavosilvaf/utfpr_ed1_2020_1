e414c7f6b8c5e96f1f0c88bc45af41a202666919
Gustavo Silva <gustavofsilva@email.com>
2020-03-19 20:16:28 -0300

    04-ex03- done

--- 04-ex3/src/com/gustavo/Main.java
+++ 04-ex3/src/com/gustavo/Main.java
@@ -1,8 +1,27 @@
 package com.gustavo;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner entrada = new Scanner(System.in);
+        String nome = entrada.nextLine();
+        String iniciais = "";
+
+
+        nome = nome.replaceAll("e |do |da |dos |das |de |di |du ", "");
+
+        System.out.println(nome);
+
+        iniciais = iniciais.concat(String.valueOf(nome.charAt(0)).toUpperCase());
+
+        for (int i = 0; i < nome.length(); i++){
+            if (nome.charAt(i) == ' '){
+                iniciais = iniciais.concat(String.valueOf(nome.charAt(i+1)).toUpperCase());
+            }
+        }
+
+        System.out.println("Inicias: " + iniciais);
     }
 }
